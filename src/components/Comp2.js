e1c13113170de8d00f9222c0e9db27980452a4cd
Saikat Chakraborty <saikatgpae@gmail.com>
2023-07-30 11:51:25 +0530

    Create the basic set up files.

---
+++ src/components/Comp2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Comp2() {
+  return (
+    <div>
+      <h1>Component 2</h1>
+    </div>
+  )
+}
---
+++ src/components/Solution.js
@@ -0,0 +1,86 @@
+// import { time } from 'console';
+import React, { useRef, useState } from 'react';
+
+
+function Solution() {
+    const [min, setMin] = useState('00');
+    const [sec, setSec] = useState('00');
+    const minInput = useRef(null);
+    const secInput = useRef(null);
+
+    // Handel the clicking of Reset button
+    const reset = () => {
+        setMin('00');
+        setSec('00');
+        minInput.current.value = '';
+        secInput.current.value = '';
+    }
+
+    const start = () => {
+        // setInterval(setSec(parseInt(sec) + 1), 1000);
+    }
+
+    // Handel min input onChange
+    const handleMin = (event) => {
+        const { value } = event.target;
+        if (value < 10){
+            setMin(String(value).padStart(2, '0'))
+        }
+        else if (value > 59) {
+            const nValue = Math.floor(value/60);
+            const remainSec = value % 60;
+            setMin(String(nValue).padStart(2, '0'));
+            setSec(String(remainSec).padStart(2, '0'));
+        }
+        else {
+            setMin(value);
+        }
+    }
+
+
+    // Handel the second input onChange
+    const handsec = (event) => {
+        const { value } = event.target;
+        if (value < 59){
+            setSec(String(value).padStart(2, '0'));
+            setMin(String(minInput.current.value).padStart(2, '0'));
+        } 
+        
+        else if (value > 59) {
+            const nValue = Math.floor(value/60);
+            const reminder = value % 60;
+            if (reminder < 10) {
+                setSec('0' + reminder);
+                setMin(parseInt(min) + nValue)
+            } else {
+                setSec(reminder);
+            }
+        }
+        
+        else {
+            setSec(value);
+        }
+    };
+
+    return (
+      <div className='p-4'>
+        <label>
+          <input type="number" onChange={handleMin} min={0} ref={minInput} autoFocus />
+          Minutes
+        </label>
+        <label>
+          <input type="number" onChange={handsec} min={0} ref={secInput} />
+          Seconds
+        </label>
+  
+        <button onClick={start}>START</button>
+        <button>PAUSE / RESUME</button>
+        <button onClick={reset}>RESET</button>
+  
+        <h1 data-testid="running-clock">{min}:{sec}</h1>
+      </div>
+    );
+  }
+  
+  export default Solution;
+  
